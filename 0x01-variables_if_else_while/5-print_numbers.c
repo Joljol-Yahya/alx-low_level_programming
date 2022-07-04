d3b583ab684f462200222397c4c44ebdc4e11afa
Joljol Yahya <joljol232@gmail.com>
2022-07-04 13:47:36 -0700

    My first commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,13 @@
 /**
  * main-Prints digits from the 10 to 0
  *
- * Return:Always 0 (Success)
+ *Return:Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 10; n++)
+	for (n = 0; n < 10; n++)
 		printf("%d\n", n);
 	
 	printf("\n");

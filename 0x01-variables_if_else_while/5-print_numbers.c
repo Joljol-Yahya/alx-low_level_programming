18aa5c2394a80e681f5047b3b0f9248467859a9c
Joljol Yahya <joljol232@gmail.com>
2022-07-04 13:58:05 -0700

    My first commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *main-Prints digits from the ten to zero
+ * main-Prints digits from the ten to zero
  *
- *Return:Always 0 (Success)
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n == 0; n < 10; n++)
 		printf("%d\n", n);	
 	printf("\n");
 
